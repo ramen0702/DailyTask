90ac3afea5cbd71f4beda4546715763894b321dc
ramen0702 <zettai72masa@gmail.com>
2022-12-08 12:03:51 +0900

    [update]UIの改善

--- DailyTask.py
+++ DailyTask.py
@@ -13,13 +13,30 @@ class App(customtkinter.CTk):
         self.geometry(f"{1100}x{580}")
         self.minsize(300, 580)
 
-        # レイアウトの設定(4×4)------------------------------------------------------------------------------------------
-        self.grid_rowconfigure((0,1,2,3), weight=0)
-        self.grid_columnconfigure((0,2), weight=1)
+        # レイアウトの設定(2×2)------------------------------------------------------------------------------------------
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
-        # 上部のバーの表示------------------------------------------------------------------------------------------
-        self.topbar_frame = customtkinter.CTkFrame(self, height=140, corner_radius=0)
-        self.topbar_frame.grid(row=0,column=0,columnspan=3,padx=50,pady=50,sticky="nsew")
+         # メニューバー(左)の表示------------------------------------------------------------------------------------------
+        menu_names = ["追加","絞り込み","一覧","修正","管理",]
+        self.menubar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.menubar_frame.grid(row=0,column=0,rowspan=2,sticky="nsew")
+        for i in range(len(menu_names)+1):
+            self.menubar_frame.grid_rowconfigure(i, weight=1)
+        # ロゴの表示
+        self.logo_label = customtkinter.CTkLabel(self.menubar_frame, text="DailyTask", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        # メニューボタンの表示
+        self.menu_button = []
+        for i in range(len(menu_names)):
+            self.menu_button.append(customtkinter.CTkButton(master=self.menubar_frame,text=menu_names[i]))
+        self.menu_button[0].configure(command=self.add_task_button)
+        for i in range(len(menu_names)):
+            self.menu_button[i].grid(row=i+1, column=0, padx=50, pady=0,)
+
+        # 日付フレーム(右上)の表示------------------------------------------------------------------------------------------
+        self.topbar_frame = customtkinter.CTkFrame(self, height=30, corner_radius=0)
+        self.topbar_frame.grid(row=0,column=1,padx=20,pady=20,sticky="nsew")
         self.topbar_frame.grid_columnconfigure(1, weight=1)
         self.topbar_frame.grid_columnconfigure((0,2), weight=0)
         # 本日の日付を表示
@@ -37,20 +54,20 @@ class App(customtkinter.CTk):
 
         # タスクの表示------------------------------------------------------------------------------------------
         self.main_frame = customtkinter.CTkFrame(self, height=140, corner_radius=0)
-        self.main_frame.grid(row=1,column=0,columnspan=2,padx=50,pady=0,sticky="nsew")
+        self.main_frame.grid(row=1,column=1,padx=20,pady=20,sticky="nsew")
         # 今は仮の辞書型を参照
-        di = {"課題":True,"例題":False,"ジム":True,"体操":False,"お買い物":True}
-        for i in range(len(di)):
-            self.main_frame.grid_columnconfigure(i, weight=1)
+        di = {"課題":True,"例題":False,"ジム":True,"体操":False,"体操1":False,"体操2":False}
+        for i in range(len(di)+1):
+            self.main_frame.grid_rowconfigure(i, weight=1)
         # 項目名の表示
-        item_names = ["達成","タスク名"]
+        item_names = ["達成　　　","タスク名"]
         self.item_label = [customtkinter.CTkLabel(
             self.main_frame
             , text=name
             , font=customtkinter.CTkFont(family="メイリオ",size=25, weight="bold"))
             for name in item_names]
         for i in range(len(item_names)):
-            self.main_frame.grid_rowconfigure(i, weight=1)
+            self.main_frame.grid_columnconfigure(i, weight=1)
             self.item_label[i].grid(row=0, column=i, padx=0, pady=5)
 
         # チェックボックスの表示
@@ -68,31 +85,7 @@ class App(customtkinter.CTk):
             , font=customtkinter.CTkFont(family="メイリオ",size=30, weight="bold"))
             for name in di]
         for i in range(len(di)):
-            self.task_label[i].grid(row=i+1, column=1, padx=0, pady=5)
-        
-
-        # トロフィー（？）の表示------------------------------------------------------------------------------------------
-        self.trophy_frame = customtkinter.CTkFrame(self, height=140, corner_radius=0)
-        self.trophy_frame.grid(row=1,column=2,columnspan=2,padx=50,pady=0,sticky="nsew")
-        # 仮テキストを表示
-        self.tmp_label = customtkinter.CTkLabel(self.trophy_frame, text="トロフィー"
-        , font=customtkinter.CTkFont(family="メイリオ",size=30, weight="bold"))
-        # self.tmp_label.grid(row=0, column=0, padx=0, pady=100,sticky="nsew")
-        self.tmp_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-        # メニューバー(下部)の表示------------------------------------------------------------------------------------------
-        menu_names = ["追加","絞り込み","一覧","修正","管理",]
-        self.menubar_frame = customtkinter.CTkFrame(self, height=140, corner_radius=0)
-        self.menubar_frame.grid(row=2,column=0,columnspan=3,padx=50,pady=50,sticky="nsew")
-        for i in range(len(menu_names)):
-            self.menubar_frame.grid_columnconfigure(i, weight=1)
-        # メニューボタンの表示
-        self.menu_button = []
-        for i in range(len(menu_names)):
-            self.menu_button.append(customtkinter.CTkButton(master=self.menubar_frame,text=menu_names[i]))
-        self.menu_button[0].configure(command=self.add_task_button)
-        for i in range(len(menu_names)):
-            self.menu_button[i].grid(row=0, column=i, padx=0, pady=5,)
+            self.task_label[i].grid(row=i+1, column=1, padx=0, pady=5)       
     
     # イベトン用関数-----------------------------------------------------------------------------------------------
     # 翌日に進むボタン
@@ -117,14 +110,24 @@ class App(customtkinter.CTk):
     
     # タスク追加ボタン
     def add_task_button(self):
-        window = customtkinter.CTkToplevel(self)
-        window.title("add task")
-        window.geometry(f"{1100}x{580}")
-        window.minsize(300, 580)
-
-        # create label on CTkToplevel window
-        label = customtkinter.CTkLabel(window, text="追加画面へようこそ")
-        label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
+        # 追加ウィンドの設定---------------------------------------------------------------------
+        # 後でやる
+        pass
+        # add_window = customtkinter.CTkToplevel(self)
+        # add_window.title("add task")
+        # add_window.geometry(f"{1100}x{580}")
+        # add_window.minsize(300, 580)
+        # # グリッドの設定(4×4)
+        # add_window.grid_rowconfigure((0,1,2,3), weight=1)
+        # add_window.grid_columnconfigure((0,1,2,3), weight=1)
+        # # タスク名入力を促すフレーム
+        # add_name_frame = customtkinter.CTkFrame(add_window, height=140, corner_radius=0)
+        # add_name_frame.grid(row=0,column=0,columnspan=4,padx=50,pady=50,sticky="nsew")
+        # add_name_frame.grid_columnconfigure(0,1, weight=1)
+        # add_name_label = customtkinter.CTkLabel(add_name_frame, text="タスク名を入力")
+        # add_name_label.grid(row=0, column=0,)
+        # add_name_entry = customtkinter.CTkEntry(add_name_frame,)
+        # add_name_entry.grid(row=0, column=1,)
 
         
 
